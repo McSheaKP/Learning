e8e072f1eed7c2247780f31abec0962dcf318498
Kevin McShea <kevinmcshea@gmail.com>
2019-02-10 18:24:28 -0800

    finished OOP section on FCC

---
+++ OOP/useIIFEToCreateAModule.js
@@ -0,0 +1,31 @@
+//Create a module named funModule to wrap the two mixins isCuteMixin and singMixin. funModule should return an object.
+
+let isCuteMixin = function(obj) {
+    obj.isCute = function() {
+      return true;
+    };
+  };
+
+  let singMixin = function(obj) {
+    obj.sing = function() {
+      console.log("Singing to an awesome tune");
+    };
+  };
+  
+
+  let funModule = (function (){
+      return {
+          isCuteMixin: function(){
+              obj.isCute = function(){
+                  return true;
+              }
+          },
+          singMixin: function(){
+              obj.sing = function(){
+                  console.log('Singing to an awesome tune')
+              }
+          }
+      }
+  }()) // 
+
+  console.log(funModule);
