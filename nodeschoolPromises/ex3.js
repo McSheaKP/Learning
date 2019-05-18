988713039b0afb9d49c92904385d1622bca77cf2
Kevin McShea <kevinmcshea@gmail.com>
2019-05-18 15:29:50 -0700

    up to exerciese 5

--- nodeschoolPromises/ex3.js
+++ nodeschoolPromises/ex3.js
@@ -1,24 +1,29 @@
-Create a promise that after a delay of 300ms, rejects with an Error object.
-The Error object should be constructed with parameter 'REJECTED!', which is
-the textual message of the error.
+// Create a promise that after a delay of 300ms, rejects with an Error object.
+// The Error object should be constructed with parameter 'REJECTED!', which is
+// the textual message of the error.
 
-Create a function onReject to print error.message using console.log. Pass
-this function as a rejection handler to the then method of your promise.
+// Create a function onReject to print error.message using console.log. Pass
+// this function as a rejection handler to the then method of your promise.
 
-## Hint
+// ## Hint
 
-As a review from last lesson, a promise's then function takes two callbacks:
-the first to be called when the promise is fulfilled and the second when the
-promise is rejected.
+// As a review from last lesson, a promise's then function takes two callbacks:
+// the first to be called when the promise is fulfilled and the second when the
+// promise is rejected.
 
-## Boilerplate
+// ## Boilerplate
 
     var promise = new Promise(function (fulfill, reject) {
       // Your solution here
-    });
-
-    function onReject (error) {
-      // Your solution here
+      setTimeout(function(){
+          reject(Error('REJECTED!'))
+      }, 300)
+    }).then(undefined, function(error){
+        onReject(error);
+    })
+    function onReject(error){
+        console.log(error.message)
     }
+
 
     // Your solution here
